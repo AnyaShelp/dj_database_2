0f490853e8a3ebebd78fcd96cf2f5e3866b993ef
AnyaShelp <anyuta.shelpyakova@gmail.com>
2023-05-20 22:55:11 +0300

    first commit

---
+++ orm_migrations/school/views.py
@@ -0,0 +1,12 @@
+from django.views.generic import ListView
+from django.shortcuts import render
+
+from .models import Student, Teacher
+
+
+def students_list(request):
+    template = 'school/students_list.html'
+    ordering = 'group'
+    students = Student.objects.order_by(ordering).prefetch_related('teachers')
+    context = {'students': students}
+    return render(request, template, context)
